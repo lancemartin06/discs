e119d40bbf662e629f10040ed8677320dc1d3d07
Martin <hitch_hiker_lm@Martins-MacBook-Pro.local>
2018-02-20 22:31:04 -0700

    Fourth Commit

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
         <link rel="stylesheet" href="mystyle.css">
     </head>
     <body>
+        <?php echo"<h1>" $currentpage "</h1>"?>
         <div id="menu">
         	 <div id = menu>
     <ul>
