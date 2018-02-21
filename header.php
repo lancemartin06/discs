50efeeffa2ab50b160b3f26958b2ecade8d66a45
Martin <hitch_hiker_lm@Martins-MacBook-Pro.local>
2018-02-20 20:47:09 -0700

    First Commit

---
+++ header.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="mystyle.css">
+    </head>
+    <body>
+        <div id="menu">
+        	<ul>
+            	<li class="menuitem"><a href="#">Home</a></li>
+                <li class="menuitem"><a href="#">About</a></li>
+                <li class="menuitem"><a href="#">Products</a></li>
+                <li class="menuitem"><a href="#">Services</a></li>
+                <li class="menuitem"><a href="#">Design</a></li>
+              <li class="menuitem"><a href="#">Contact</a></li>
+            </ul>
+        </div>
+        
+    
