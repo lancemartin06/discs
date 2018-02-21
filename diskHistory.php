337efd33bc3d24fbc06ed39a961f828f6400ef41
Martin <hitch_hiker_lm@martins-mbp.boisestate.edu>
2018-02-21 11:47:21 -0700

    Updated Nav Bar and Logo

--- diskHistory.php
+++ diskHistory.php
@@ -6,7 +6,7 @@ require_once("header.php");
     
         <section class="sec1"></section>
         <section class="content">
-            <p>.</p>
+            <iframe id="iframe_container" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" width="550" height="400" src="https://prezi.com/embed/4araxu9upkib/?bgcolor=ffffff&amp;lock_to_path=0&amp;autoplay=0&amp;autohide_ctrls=0&amp;landing_data=bHVZZmNaNDBIWnNjdEVENDRhZDFNZGNIUE43MHdLNWpsdFJLb2ZHanI0ejBqQ1NKakNsdXRsenlMcjRjM2hHM2NBPT0&amp;landing_sign=DCyinGic8ClolYUq2CgacRaI5HKeON7ytkDtNvEe_Is"></iframe>
         </section>
     
         
