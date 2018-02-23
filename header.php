966fc88265abbb9ded356cecc62429231df777de
Martin <hitch_hiker_lm@martins-mbp.boisestate.edu>
2018-02-23 11:48:28 -0700

    Update

--- header.php
+++ header.php
@@ -10,10 +10,9 @@
         <nav class='black'>
             <div class = "logo"><a href = "home.html"><img src="Pictures/untitled4.png" align="left"></a></div>
             <ul>
-                <li><a <?php if(PAGENAME == "diskHistory"){ ?> class =active <?php } ?> href="diskHistory.php">Disk History</a></li>
-                <li><a <?php if(PAGENAME == "courses"){ echo " class =\"active\" "} ?> href="courses.php">Boise Courses</a></li>
-                <li><a <?php if(PAGENAME == "starting"){ echo " class =\"active\" "} ?> href="starting.php">Get Started</a></li>
-                <li><a <?php if(PAGENAME == "lostDiscs"){ echo " class =\"active\" "} ?> href="lostDiscs.php">Lost Discs</a></li>
+                <li><a <?php if(PAGENAME == "courses"){ echo " class =\"active\" ";} ?> href="courses.php">Boise Courses</a></li>
+                <li><a <?php if(PAGENAME == "starting"){ echo " class =\"active\" ";} ?> href="starting.php">Get Started</a></li>
+                <li><a <?php if(PAGENAME == "lostDiscs"){ echo " class =\"active\" ";} ?> href="lostDiscs.php">Lost Discs</a></li>
             </ul>
         </nav>
         
