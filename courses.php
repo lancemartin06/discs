dd767ce56f7080588fdcaa80804a212d154008e4
Martin <hitch_hiker_lm@martins-mbp.boisestate.edu>
2018-03-05 12:51:29 -0700

    Updated Course Page

--- courses.php
+++ courses.php
@@ -8,10 +8,18 @@
 
 <section class="sec1"><p>GOOGLE MAP</p></section>
         <section class="content">
-            <p><h1>Anne Morrison</h1>
+            <pre><h1>Anne Morrison</h1>
                 <img src = "Pictures/Courses/annemorrison.jpg">
                 Anne Morrison park is a great park to disc golf. It supports a large course with many challenging and obstacles. The park is very friendly to disc golfers and is very popular to many people in the communitiy. 
-            </p>
+            </pre>
+        </section>
+        <section class="content">
+            <pre><h1>Bogus Basin</h1>
+                <img src = "Pictures/Courses/annemorrison.jpg">
+                Bogus Basin is normally thought of as a ski resort, but in the summer time, it becomes
+                incredible scene for disc golf. It is much more challenging than your average course, 
+                it offers a lot more diversity and unforgettable scenery. 
+            </pre>
         </section>
     
         
