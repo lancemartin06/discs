8230a17e4a041fc3b1662bd6c091095a7ec53b0e
Martin <hitch_hiker_lm@martins-mbp.boisestate.edu>
2018-03-05 17:22:28 -0700

    Updated Course Page

--- courses.php
+++ courses.php
@@ -3,10 +3,24 @@
 
     require_once("header.php");
 
+}
+
 ?>
 <title>Courses</title>
 
-<section class="sec1"><div class = "firstCenter"><p>GOOGLE MAP</p></div></section>
+<script>
+function myMap() {
+var mapOptions = {
+    center: new google.maps.LatLng(43.612, -116.22),
+    zoom: 10,
+    mapTypeId: google.maps.MapTypeId.HYBRID
+}
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+</script>
+
+<section class="sec1"><div class = "firstCenter"><div id="map"><script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBBRx6Uyqvmt-D3yNIGhsfYUgBlUj6J8f0=myMap"></script>
+    </div></div></section>
         <section class="content">
             <pre><h1>Anne Morrison</h1>
                 <img src = "Pictures/Courses/annemorrison.jpg">
@@ -19,8 +33,9 @@
         <section class="content">
             <pre><h1>Bogus Basin</h1>
                 <img src = "Pictures/Courses/bogus.jpg">
-                Bogus Basin is normally thought of as a ski resort, but in the summer time, it becomes
-                incredible scene for disc golf. It is much more challenging than your average course, 
+                Bogus Basin is normally thought of as a ski resort, but in 
+                the summer time, it becomes incredible scene for disc golf. It 
+                is much more challenging than your average course, 
                 it offers a lot more diversity and unforgettable scenery. 
             </pre>
         </section>
