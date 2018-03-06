912aba5bf481a5b827dfbb29fd11bdab7067f702
Martin <hitch_hiker_lm@martins-mbp.boisestate.edu>
2018-03-05 17:37:38 -0700

    Updated Course Page

--- courses.php
+++ courses.php
@@ -7,8 +7,13 @@
 <title>Courses</title>
 
 
-<section class="sec1"><div class = "firstCenter"><div id="map">
-    </div></div></section>
+<section class="sec1">
+    <div class = "firstCenter">
+        <div id="map">
+        </div>
+    </div>
+</section>
+
 <script>
 function myMap() {
 var mapOptions = {
@@ -19,7 +24,7 @@ var mapOptions = {
 var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
 </script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBBRx6Uyqvmt-D3yNIGhsfYUgBlUj6J8f0=myMap"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBBRx6Uyqvmt-D3yNIGhsfYUgBlUj6J8f0&callback=myMap"></script>
 
         <section class="content">
             <pre><h1>Anne Morrison</h1>
