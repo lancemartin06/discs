473965a1deacfeccc6586df29c92f1b34bcd31b6
Martin <hitch_hiker_lm@martins-mbp.boisestate.edu>
2018-03-26 13:19:43 -0600

    Update?

--- lostDiscs.php
+++ lostDiscs.php
@@ -1,15 +1,50 @@
-<?php
+
+<?php 
+
+    require 'db.php';
+    session_start();
 
     define("PAGENAME", "lostDiscs");
 
     require_once("header.php");
-
+    
 ?>
+    <link rel="stylesheet" href="CSS/mystyle2.css">
     <title>Lost Discs</title>
         <section class="sec1"></section>
-        <section class="content">
-            <p>Imma log you in dawg!</p>
-        </section>
+        <div class="form">
+      
+      <ul class="tab-group">
+        <li class="tab"><a href="#signup">Sign Up</a></li>
+        <li class="tab active"><a href="#login">Log In</a></li>
+      </ul>
+      
+      <div class="tab-content">
+
+         <div id="login">   
+          <h1>Welcome Back!</h1>
+          
+          <form action="index.php" method="post" autocomplete="off">
+          
+            <div class="field-wrap">
+            <label>
+              Email Address<span class="req">*</span>
+            </label>
+            <input type="email" required autocomplete="off" name="email"/>
+          </div>
+          
+          <div class="field-wrap">
+            <label>
+              Password<span class="req">*</span>
+            </label>
+            <input type="password" required autocomplete="off" name="password"/>
+          </div>
+          
+          <p class="forgot"><a href="forgot.php">Forgot Password?</a></p>
+          
+          <button class="button button-block" name="login" />Log In</button>
+          
+          </form>
     
         
     </div>
