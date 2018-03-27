e1ff3c43915302c6fb805a3f7555911e894f6c3c
Martin <hitch_hiker_lm@Martins-MacBook-Pro.local>
2018-03-26 21:23:21 -0600

    Update with DB

--- lostDiscs.php
+++ lostDiscs.php
@@ -9,8 +9,8 @@
 ?>
     <link rel="stylesheet" href="CSS/mystyle2.css">
     <title>Lost Discs</title>
-        <section class="sec1"></section>
-        <div class="form">
+        <section class="sec1">
+    <div class="form">
       
       <ul class="tab-group">
         <li class="tab"><a href="#signup">Sign Up</a></li>
@@ -43,8 +43,55 @@
           <button class="button button-block" name="login" />Log In</button>
           
           </form>
-    
+
+        </div>
+          
+        <div id="signup">   
+          <h1>Sign Up for Free</h1>
+          
+          <form action="index.php" method="post" autocomplete="off">
+          
+          <div class="top-row">
+            <div class="field-wrap">
+              <label>
+                First Name<span class="req">*</span>
+              </label>
+              <input type="text" required autocomplete="off" name='firstname' />
+            </div>
         
+            <div class="field-wrap">
+              <label>
+                Last Name<span class="req">*</span>
+              </label>
+              <input type="text"required autocomplete="off" name='lastname' />
+            </div>
+          </div>
+
+          <div class="field-wrap">
+            <label>
+              Email Address<span class="req">*</span>
+            </label>
+            <input type="email"required autocomplete="off" name='email' />
+          </div>
+          
+          <div class="field-wrap">
+            <label>
+              Set A Password<span class="req">*</span>
+            </label>
+            <input type="password"required autocomplete="off" name='password'/>
+          </div>
+          
+          <button type="submit" class="button button-block" name="register" />Register</button>
+          
+          </form>
+
+        </div>  
+        
+      </div><!-- tab-content -->
+      
+</div> 
+        
+        </section>
     </div>
 
  <?php require_once("footer.php"); ?>
