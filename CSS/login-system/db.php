3f8061145b4936302692bbd563dc283a4ac5eac3
Martin <hitch_hiker_lm@martins-mbp.boisestate.edu>
2018-03-27 11:19:18 -0600

    Updated Login

--- CSS/login-system/db.php
+++ CSS/login-system/db.php
@@ -1,7 +1,7 @@
 <?php
 /* Database connection settings */
-$host = 'localhost';
-$user = 'root';
-$pass = 'mypass123';
-$db = 'accounts';
+$host = 'usl-cdbr-iron-eas-05.cleardb.net';
+$user = 'bcc29ebdb3e631';
+$pass = '0a186730';
+$db = 'heroku_460fd0693927d5a';
 $mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
--- lostDiscs.php
+++ lostDiscs.php
@@ -1,7 +1,7 @@
 
 <?php 
     session_start();
-
+    require_once("header.php");
     define("PAGENAME", "lostDiscs");
     
 ?>
