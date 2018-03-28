2a36ef1789838eb0c2d3a2d4eca63c331924b085
Martin <hitch_hiker_lm@Martins-MacBook-Pro.local>
2018-03-27 19:56:13 -0600

    update

--- lostDiscs.php
+++ lostDiscs.php
@@ -101,7 +101,49 @@
         <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
         
         
-      <script src="lostDiscs.js"></script>
+      <script>$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+            var $this = $(this),
+            label = $this.prev('label');
+
+                  if (e.type === 'keyup') {
+                        if ($this.val() === '') {
+                      label.removeClass('active highlight');
+                    } else {
+                      label.addClass('active highlight');
+                    }
+                } else if (e.type === 'blur') {
+                    if( $this.val() === '' ) {
+                        label.removeClass('active highlight'); 
+                        } else {
+                        label.removeClass('highlight');   
+                        }   
+                } else if (e.type === 'focus') {
+
+                  if( $this.val() === '' ) {
+                        label.removeClass('highlight'); 
+                        } 
+                  else if( $this.val() !== '' ) {
+                        label.addClass('highlight');
+                        }
+                }
+
+            });
+
+        $('.tab a').on('click', function (e) {
+  
+          e.preventDefault();
+
+          $(this).parent().addClass('active');
+          $(this).parent().siblings().removeClass('active');
+
+          target = $(this).attr('href');
+
+          $('.tab-content > div').not(target).hide();
+
+          $(target).fadeIn(600);
+
+        });</script>
 
         </div>  
         
