73d96c5c84e620cb0f7e12d13dce995804334f7e
Martin <hitch_hiker_lm@Martins-MacBook-Pro.local>
2018-03-27 19:07:05 -0600

    update

--- lostDiscs.php
+++ lostDiscs.php
@@ -98,11 +98,11 @@
         
         
         
-        <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'>
+        <script src='http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
         
         
         
-      
+      <script>
             $('.form').find('input, textarea').on('keyup blur focus', function (e) {
   
             var $this = $(this),
