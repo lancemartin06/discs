f8ab3df13bd8ad88dc4a077178e3d3cf0e5b9a27
Martin <hitch_hiker_lm@Martins-MacBook-Pro.local>
2018-03-30 10:30:09 -0600

    update

--- lostDiscs.php
+++ lostDiscs.php
@@ -3,7 +3,22 @@
     session_start();
     define("PAGENAME", "lostDiscs");
     require_once("header.php");
-    include("dao.php");
+    //include("dao.php");
+$servername = "us-cdbr-iron-east-05.cleardb.net";
+$dbname = "heroku_460fd0693927d5a";
+$username = "bcc29ebdb3e631";
+$password = "0a186730";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
    
     
 ?>
