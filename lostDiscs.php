172245429c7fbd3bcd9746e366b753d5c5475a4e
Martin <hitch_hiker_lm@martins-mbp.boisestate.edu>
2018-03-31 12:32:00 -0600

    update

--- lostDiscs.php
+++ lostDiscs.php
@@ -1,7 +1,50 @@
 <?php
-define("PAGENAME", "lostDiscs");
-require('dao.php');
 session_start();
+define("PAGENAME", "lostDiscs");
+//require('dao.php');
+
+
+class Dao 
+{
+    private $servername = "us-cdbr-iron-east-05.cleardb.net";
+    private $dbname = "heroku_460fd0693927d5a";
+    private $username = "bcc29ebdb3e631";
+    private $password = "0a186730";
+
+    public function getConnection()
+    {
+        echo "getting connection!";
+        return new PDO("mysql:host={$this->servername};dbname={$this->dbname}", $this->username, $this->password);
+    }
+    
+    public function addUser($email, $pass, $name, $phone)
+    {
+        
+        $conn = getConnection();
+        
+        $result = $conn->query("SELECT * FROM user WHERE email='$email'");
+
+        // If the result is greater than 0 than the user already exists. 
+        if ($result > 0) {
+        echo('User with this email already exists!');
+        } else {
+            $sql = "INSERT INTO user (email, password, name, phone) VALUES ('$email','$password','$name','$phone')";
+            if ($conn->query($sql)) {
+                echo "You're All Signed Up!";
+            } else {
+                echo "Oh no. Something Went Wrong.";
+            }
+        }
+    }
+    
+    public function getUser($email, $pass) {
+
+    }
+}
+
+
+
+
 $dao = new Dao();
 require_once'header.php';
 
