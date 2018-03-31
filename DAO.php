9240e0e01ae7b442c4b8f9da23e495ac8244176c
Martin <hitch_hiker_lm@martins-mbp.boisestate.edu>
2018-03-30 18:55:22 -0600

    update

--- DAO.php
+++ DAO.php
@@ -1,20 +1,20 @@
 <?php
-class DAO {
+class Dao {
 
-private $servername = "us-cdbr-iron-east-05.cleardb.net";
-private $dbname = "heroku_460fd0693927d5a";
-private $username = "bcc29ebdb3e631";
-private $password = "0a186730";
+    private $servername = "us-cdbr-iron-east-05.cleardb.net";
+    private $dbname = "heroku_460fd0693927d5a";
+    private $username = "bcc29ebdb3e631";
+    private $password = "0a186730";
 
-    public function getConnection() {
+public function getConnection() {
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Good Connection!";
     }
     catch (PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
     }
-
     return $conn;
 }
    
@@ -29,9 +29,9 @@ private $password = "0a186730";
         } else {
             $sql = "INSERT INTO user (email, password, name, phone) VALUES ('$email','$password','$name','$phone')";
             if ($conn->query($sql)){
-                echo "You're All Signed Up!"
+                echo "You're All Signed Up!";
             } else {
-                echo "Oh no. Something Went Wrong."
+                echo "Oh no. Something Went Wrong.";
             }
         }
     }
@@ -40,4 +40,3 @@ private $password = "0a186730";
 
     }
 }
-?>
