32550bd8b50c7490415c8ebb7a2e6f969a4faa15
Lance Martin <lancemartin@u.boisestate.edu>
2018-04-02 18:58:53 -0600

    update

--- CSS/login-system/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* Database connection settings */
-$host = 'usl-cdbr-iron-eas-05.cleardb.net';
-$user = 'bcc29ebdb3e631';
-$pass = '0a186730';
-$db = 'heroku_460fd0693927d5a';
-$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
