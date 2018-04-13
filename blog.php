e45529d7fe1fa3ed5dc9fd6c5e0026f1dd79b5f8
Lance Martin <lancemartin@u.boisestate.edu>
2018-04-13 13:09:21 -0600

    update

--- blog.php
+++ blog.php
@@ -8,7 +8,63 @@
         
         <section class="sec1">Bloggin'</section>
         <section class="content">
-            <p>.</p>
+            <h1>AI and the future Workforce</h1>
+            <pre>   There has been a lot of worry in the media about artificial intelligence and how it’s going to
+                change our lives. We seem to have this perception that we are on an unstoppable technology train and
+                there is no stopping what is coming. So, what’s coming? Oh, just a man made all knowing computer god
+                that will take away jobs from people and destroy the lives of the working man, but let’s be honest.
+                This is never going to happen. We have seen a trend in automation in the workforce, and we know this
+                is coming. Through this blog, I’d like to take a look at this, “unstoppable tech train”, and try to
+                determine if it is riding on ethical rails.
+
+                    First I’d like to look at the positives effects of using artificial intelligence in the workforce.
+                There have been some pretty positive changes in the few years with automation.
+
+                    I’ll begin with a few personal examples from my perspective. One of the most helpful advances has
+                been in checkout lines for example. To be honest some of the most miserable people I have to deal with
+                in my daily life are cashiers at Walmart. And the reality is that when I have to buy condoms, I prefer
+                to not look into the judgmental eyes of Ethel, the 67 year old cashier that is less than excited to see
+                me when I stand in her line at the checkout. Freeing up this unpopular occupations gives people the
+                opportunity to do other tasks. There are many jobs in the market today that could benefit from AI.
+                I believe that there will always be a need for human intelligence, though limited in many.
+
+                    Japan has always strived to be a leader in implementing new technology. Right now they are using
+                a friendly robot called, Pepper. Pepper is being used in over 140 SoftBank Mobile stores. Pepper is
+                able to read emotions using vocal patterns and body language. Pepper can help the customers with a
+                variety of tasks such as providing information, directions, and carry on basic conversation. This is an
+                incredibly helpful tool for many businesses.
+
+                    Our old buddy Amazon is already using state of the art robotics to retrieve boxes from their
+                warehouses. Every robot is paired with a human counterpart. These robots quickly locate and retrieve
+                crates with items in them. Though these robots may be efficient they cannot identify the difference
+                between items that are in crate. That’s where the human comes in. Amazon is pushing for human-robotic
+                teamwork. The people working in Amazon’s warehouses now have better jobs because of AI. (Amazon Robotics)
+
+                    We have all seen self-driving cars. This type of AI is just on our horizon. When people say it makes
+                them uncomfortable. Imagine this… you can get an Uber, but without the driver!
+
+                    These all seem like positive examples of implement AI in the workforce, but now we need to see
+                these situations from the perspective of the people. The biggest concerns is, what will happen to
+                our jobs?
+
+                    Well, some jobs will be at risk. A study was done in 2013 by Carl Frey and Michael Osborne, stated
+                47% of jobs may be at risk of being replaced by automation (Frey 2013).  That’s an astounding amount.
+                In 2016, Council of Economic Advisors (CEA) determined that 83% of people with an occupation that is
+                making less than twenty dollars per hour would be at risk. Positions making around twenty-forty an hour
+                have a 31% risk, and everyone making more than the would be staring down a 4% risk. So, if you are seeing
+                this, you might notice that’s mostly everyone. (Executive 2016).
+
+                    If we are looking at this the data the wrong way, we will say this is unethical. If you think it’s
+                unethical, you’ll say that people are going to suffer, we will be poverty stricken, and AI is going to
+                bring suffering.
+
+                    You might be right, but lets look at that report from the CEA again. That report tells me that 47%
+                of jobs can be be replaced by AI, that means their jobs are probably shit. I believe we need to expect
+                more out of people. Lets see this as an opportunity to purge our lives of mundane and monotonous jobs.
+                Maybe we need this revolution more than we need our menial paycheck. People are resilient and you’d be
+                daft to think that AI wouldn’t force us to become more innovative. Let’s just acknowledge that the
+                industrial revolution didn’t kill agriculture, but enhanced it. Jobs will change and they always have.
+                So, I have to say that change can be uncomfortable and hard, but it doesn’t mean it’s unethical.</pre>
         </section>
     
         
