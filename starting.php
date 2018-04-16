910214c3190075928813ffc278b457a61798083e
Lance Martin <lancemartin@u.boisestate.edu>
2018-04-16 12:55:55 -0600

    update

--- starting.php
+++ starting.php
@@ -5,13 +5,71 @@
 
 ?>
     <title>Beginner's Guide</title>
-        
-        <section class="sec1">You're gonna learn today!</section>
-        <section class="content">
-            <a href="blog.php">blog</a>
-            <p>.</p>
-        </section>
-    
+
+    <section class="content">
+            <pre><h1>Beginner's Guide</h1>
+
+                This is a simple guide to help anyone who is new to Disc Golf to get
+                started with some helpful hints and tips!
+            </pre>
+    </section>
+    <section class="content">
+            <pre><h1>1. Learn the Technique</h1>
+
+                    Take time to learn how to throw. Start with the basics and work your way from there.
+                    Don't feel bad because you can't rip it like the best of them. Many people need time
+                    to learn how their own body's style. Everyone is different and there
+                    are many sizes and shapes of discs, so it may take time to find one that works for you.
+
+            </pre>
+    </section>
+    <section class="content">
+            <pre><h1>2. Practice</h1>
+
+                    Learn to throw. Flat and low are the essential cornerstones
+                for any disc golfer. You need to establish a routine for developing
+                a good technique. If you have a hyperactive pup at home, this may be
+                a great way to kill two birds with one stone.
+
+                    Make sure to stretch before and after. Your rotator cuff is a beautiful
+                thing and expensive to fix.
+
+            </pre>
+    </section>
+
+    <section class="content">
+            <pre><h1>3. Play With Better Golfers</h1>
+
+                    Perfecting your technique will have to come from feedback
+                from experienced players. You'll find this is good advice for life in
+                general. You'll probably lose a lot, but you'll improve much quicker than
+                playing alone.
+
+            </pre>
+    </section>
+
+    <section class="content">
+            <pre><h1>4. Fewer Discs at First</h1>
+
+                    You might be tempted to buy very quality discs right away. Try tor resist
+                this urge. Develop the basics on the basics. Stick with starter discs until you
+                have developed your abilities. If you are playing with more experienced players,
+                they are likely to have nice discs for you to try out anyway.
+
+            </pre>
+    </section>
+    <section class="content">
+            <pre><h1>5. Have Fun.</h1>
+
+                    Try to have fun and don't take yourself too seriously when you are
+                starting out! It's more fun when you can laugh at yourself. Lower expectations
+                are the key to happiness.
+
+                Also, take it easy around water features. If you do find yourself with a disc in
+                the darkness of the pond, we'll be there to help you out! If you see us out there,
+                feel free ask us to get your discs.
+            </pre>
+    </section>
         
     </div>
 
