ee538d3fa822641de54c6e9f94339528d03f7ee2
Lance Martin <lancemartin@u.boisestate.edu>
2018-04-16 11:53:08 -0600

    update

--- diskHistory.php
+++ diskHistory.php
@@ -5,9 +5,10 @@
 <title>History</title>
     
         <section class="sec1">
-        <section class="content">
             <iframe id="iframe_container" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" width="650" height="500" src="https://prezi.com/embed/gj82xwdmizd_/?bgcolor=ffffff&amp;lock_to_path=1&amp;autoplay=0&amp;autohide_ctrls=0&amp;landing_data=bHVZZmNaNDBIWnNjdEVENDRhZDFNZGNIUE43MHdLNWpsdFJLb2ZHanI0a2ZHU0ZvVVJRZjhGUWM0R1FrT0VvMHVBPT0&amp;landing_sign=3G72QcwXepg95zEeVIy9fLpOYqyXPQd_MZ4m087Zaew"></iframe>
-            </section>
+        </section>
+        <section class="content">
+        </section>
         </section>
     
         
