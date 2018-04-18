ad9d2615a575d16dc8f294d25e2c6857ae4add61
Lance Martin <lancemartin@u.boisestate.edu>
2018-04-18 11:58:01 -0600

    Update

--- lostDiscs.php
+++ lostDiscs.php
@@ -18,7 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
         $dao->getUser($_POST['email'], $_POST['password']);
 
         if($_SESSION['logged_in'] === true){
-            //header('Location: profile.php');
+            header('Location: profile.php');
         }
         else {
             header('Location: lostDiscs.php');
